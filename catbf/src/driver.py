79a711a64246bdfc0ae874fb9815f59a0d697ab8
Damian Ho <damian_ho_xu_yang@yahoo.com>
2022-04-05 18:39:38 -0400

    Centralized configs

--- catbf/src/driver.py
+++ catbf/src/driver.py
@@ -2,24 +2,34 @@ import os
 import sys
 import filecmp
 
+# CONFIGS
+MAX_SCORE = 100
+STUDENT_FILE = "cat.bf"
+SOLUTION_FILE = "cat-sol.bf"
+STUDENT_OUTPUT = "output.txt"
+SOLUTION_OUTPUT = "output-sol.txt"
+
+# CODE
 input_files = list(filter(lambda x: (x.endswith(".in")), os.listdir("./inputs")))
 
 tot = len(input_files)
 correct = 0
-max_score = 100
 
 for input_f in input_files:
     path = "./inputs/" + input_f
     print("Running input {}".format(path))
-    os.system("beef cat.bf -i {} -o output.txt".format(path))
-    os.system("beef cat-sol.bf -i {} -o output-sol.txt".format(path))
-    if filecmp.cmp("output.txt", "output-sol.txt"):
-        print("... Correct on input {}".format(input_f))
+    os.system("beef {} -i {} -o {}".format(STUDENT_FILE, path, STUDENT_OUTPUT))
+    os.system("beef {} -i {} -o {}".format(SOLUTION_FILE, path, SOLUTION_OUTPUT))
+    if filecmp.cmp(STUDENT_FILE, SOLUTION_FILE):
+        print("... Correct on {}".format(input_f))
         correct += 1
     else:
-        print("... Wrong on input {}".format(input_f))
+        print("... Wrong on {}".format(input_f))
 
-print("Total inputs: {}".format(tot))
-print("Correct inputs: {}".format(correct))
+os.remove(STUDENT_FILE)
+os.remove(SOLUTION_FILE)
+print("Cleaned up files")
 
-print("{{\"scores\": {{\"Correctness\":{}}}}}".format(max_score * (correct // tot)))
+print("Correct inputs: {} / {}".format(correct, tot))
+
+print("{{\"scores\": {{\"Correctness\":{}}}}}".format(MAX_SCORE * (correct // tot)))
