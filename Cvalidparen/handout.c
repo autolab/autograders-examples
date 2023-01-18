e425fc3439a62bc65a1e42169135d8c26115dc5a
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2023-01-17 20:34:15 -0500

    Add valid parenthesis lab

---
+++ Cvalidparen/handout.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/**
+ * Returns whether `parenString` has valid parentheses. 
+ *
+ * A string has valid parentheses if:
+ * - Each open parenthesis has a corresponding close parenthesis.
+ * - Open parentheses are closed in the correct order.
+ * - There are no extra close parentheses. 
+ * 
+ * @pre The only characters in `parenString` are '(' and ')'.
+ */
+bool validParenthesis(const char *parenString) {
+    // Write your code here!
+    return false;
+}
