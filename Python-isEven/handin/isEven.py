dfd8943838efb1f4d258398058e7b21e23590d2f
Joy Song <joysong@Joys-MacBook-Air-2.local>
2024-01-27 15:44:05 -0500

    added isEven files

---
+++ Python-isEven/handin/isEven.py
@@ -0,0 +1,2 @@
+def isEven(x):
+  return (x%2==0)
---
+++ Python-isEven/src/tests.py
@@ -0,0 +1,14 @@
+'''
+Custom test cases can be written below.
+
+Each test case is formatted as a list in the following order:
+0. Name for test case
+1. Valid Input (integer)
+2. Points rewarded
+'''
+
+testCases = [
+  ["Test 1", 72, 1],
+  ["Test 2", -13, 1],
+  ["Test 3", 0, 1],
+]
